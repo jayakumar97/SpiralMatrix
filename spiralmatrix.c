3be6572fd9c82f259b58b00a024edd9da68ac93d
jayakumar97 <rjkhema14@gmail.com>
2017-09-01 23:02:23 +0530

    Update spiralmatrix.c

--- spiralmatrix.c
+++ spiralmatrix.c
@@ -2,38 +2,59 @@
 int main()
 {
 int a[100][100],i=0,j=0,n,k;
+    printf("Enter N ");
 scanf("%d",&n);
 int rmax,cmax;
 rmax=cmax=n;
 int len=n*n;
+    //loop to access each outerlayer of the matrix towards center
+/*if n=5
+ # # # # (#)->cmax
+ # ^ ^ ^ #
+ # ^ $ ^ #  
+ # ^ ^ ^ #
+(#)# # # #
+ |
+ \/
+ rmax
+ #->1st square
+ ^->2nd square
+*/
 for(k=1;k<=n*n;rmax--,cmax--)
 {
+ //loop to assign num column wise from first to last column
 for(j=n-cmax;j<cmax;j++,k++)
 {
     a[i][j]=k;
-  //  printf("(%d,%d)=%d,",i,j,k);
+  
 }
 
 j--;
 i++;
+    
+ //loop to assign num row wise from first to last row
 for(i=n-rmax+1;i<rmax;i++,k++)
 {
     a[i][j]=k;
-    //printf("(%d,%d)=%d,",i,j,k);
+    
 }
 j--;
 i--;
+    
+ //loop to assign num column wise from last to first column
 for(;j>=n-cmax;j--,k++)
 {
     a[i][j]=k;
-    //printf("(%d,%d)=%d,",i,j,k);
+   
 }
 j++;
 i--;
+    
+ //loop to assign num column wise from last to first row
 for(;i>n-rmax;i--,k++)
 {
     a[i][j]=k;
-    //printf("(%d,%d)=%d,",i,j,k);
+    
 }
 i++;
 
